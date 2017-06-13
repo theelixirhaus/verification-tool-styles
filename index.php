d4ccf6ccc5d400d776c9694a196ebb6917a80f6b
Rick <rick@modernpostmodern.com>
2017-06-12 20:17:50 -0400

    Initital Commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+    
+        <!-- Pure CSS Used for Grid and Lyout -->
+        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <p>Hello world! This is HTML5 Boilerplate.</p>
+
+        <div class="pure-g">
+            <div class="pure-u-1-3"><p>Thirds</p></div>
+            <div class="pure-u-1-3"><p>Thirds</p></div>
+            <div class="pure-u-1-3"><p>Thirds</p></div>
+        </div>
+
+        <script src="js/main.js"></script>
+    </body>
+</html>
